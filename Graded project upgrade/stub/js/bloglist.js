747fa96a996a0c81287a037513d41e03c74976dc
sachit5 <49784604+sachit5@users.noreply.github.com>
2019-07-14 23:39:43 +0530

    Add files via upload

---
+++ Graded project upgrade/stub/js/bloglist.js
@@ -0,0 +1,6 @@
+var trashModal = document.getElementById("trashModal");
+var noBuuton = document.getElementById("No");
+
+function trashShow() { trashModal.style.display="block";}
+
+noBuuton.onclick=function () {  trashModal.style.display="none"; }
